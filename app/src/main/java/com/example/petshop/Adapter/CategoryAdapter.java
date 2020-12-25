7be5a7299964f6714d3711fd176eb1193716c823
Nguyên <huynhquocnguyen99@gmail.com>
2020-12-25 19:32:29 +0700

    add code category fragment

---
+++ app/src/main/java/com/example/petshop/Adapter/CategoryAdapter.java
@@ -0,0 +1,57 @@
+package com.example.petshop.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.petshop.Class.Category;
+import com.example.petshop.R;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private Context mContext;
+    private ArrayList<Category> mCategory;
+
+    public CategoryAdapter(Context mContext, ArrayList<Category> mCategory) {
+        this.mContext = mContext;
+        this.mCategory = mCategory;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(mContext);
+        View categoryView=inflater.inflate(R.layout.item_category,parent,false);
+        ViewHolder viewHolder=new ViewHolder(categoryView);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Category category=mCategory.get((position));
+        holder.txtCategory.setText(category.getNameCategory());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCategory.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imgCategory;
+        private TextView txtCategory;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgCategory = itemView.findViewById(R.id.imgCategory);
+            txtCategory = itemView.findViewById(R.id.txtCategory);
+        }
+    }
+}
