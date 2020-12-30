d95cf78379a956679a53c58096191d956c7ffc2d
tvn0122 <ntat0013@gmail.com>
2020-12-30 20:16:06 +0700

    add category to home-fragment

--- app/src/main/java/com/example/petshop/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/petshop/Fragment/HomeFragment.java
@@ -3,64 +3,81 @@ package com.example.petshop.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.Toast;
 
+import com.example.petshop.Adapter.CategoryAdapter;
+import com.example.petshop.Adapter.ProductInCategoryAdapter;
+import com.example.petshop.Class.Category;
+import com.example.petshop.Class.Product;
 import com.example.petshop.R;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
+
+
 public class HomeFragment extends Fragment {
+    private View viewRoot;
+    private RecyclerView homeCategoryRcv;
+    private ArrayList<Category> listCategory = new ArrayList<>();
+    private Category category;
+    private CategoryAdapter adapter = null;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        viewRoot = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Init();
+        return viewRoot;
     }
+
+    public void getDataCategory() {
+        db = FirebaseFirestore.getInstance();
+        db.collection("category")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w("Fail", "Listen failed.", e);
+                            return;
+                        }
+                        for (QueryDocumentSnapshot doc : value) {
+                            listCategory.add(new Category(doc.getId().toString(), doc.get("categoryName").toString(), doc.get("imageCategory").toString()));
+                            adapter.notifyDataSetChanged();
+                        }
+                        Log.d("TAG", "Current cites in CA: ");
+                    }
+                });
+    }
+
+    private void Init() {
+        homeCategoryRcv = (RecyclerView) viewRoot.findViewById(R.id.homeCategoryRcv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        getDataCategory();
+        Log.d("hehe", String.valueOf(listCategory.size()));
+        adapter = new CategoryAdapter(getContext(), null, listCategory);
+        homeCategoryRcv.setLayoutManager(layoutManager);
+        homeCategoryRcv.setAdapter(adapter);
+
+    }
+
+
 }
