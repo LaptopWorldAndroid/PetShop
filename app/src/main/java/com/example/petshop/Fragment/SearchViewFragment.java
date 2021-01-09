567f7cb612f48504b0726e7f15fea4259ddef1b8
Viet Phi Nguyen <vietphi99dh@gmail.com>
2021-01-10 04:07:12 +0700

    fix commit

--- app/src/main/java/com/example/petshop/Fragment/SearchViewFragment.java
+++ app/src/main/java/com/example/petshop/Fragment/SearchViewFragment.java
@@ -53,8 +53,6 @@ public class SearchViewFragment extends Fragment {
         return viewRoot;
     }
 
-
-
 //    private void search(String s){
 //        ArrayList<Product>mylist=new ArrayList<>();
 //
